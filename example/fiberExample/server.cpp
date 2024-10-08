8309af6a97a43e00af9ba76415260d3a2f80334b
Emperor-WE <1966573861@qq.com>
2024-10-08 06:51:37 +0000

    [EXAMPLE][测试用例调整]

--- example/fiberExample/server.cpp
+++ example/fiberExample/server.cpp
@@ -13,75 +13,102 @@ static int listen_sock = -1;
 void test_accept();
 
 // task
-void watch_io_read() { monsoon::IOManager::GetThis()->addEvent(listen_sock, monsoon::READ, test_accept); }
-
-void test_accept() {
-  struct sockaddr_in addr;
-  memset(&addr, 0, sizeof(addr));
-  socklen_t len = sizeof(addr);
-  int fd = accept(listen_sock, (struct sockaddr *)&addr, &len);
-  if (fd < 0) {
-    std::cout << "fd = " << fd << ",accept error" << std::endl;
-  } else {
-    std::cout << "fd = " << fd << ",accept success" << std::endl;
-    fcntl(fd, F_SETFL, O_NONBLOCK);
-    monsoon::IOManager::GetThis()->addEvent(fd, monsoon::READ, [fd]() {
-      char buffer[1024];
-      memset(buffer, 0, sizeof(buffer));
-      while (true) {
-        int ret = recv(fd, buffer, sizeof(buffer), 0);
-        if (ret > 0) {
-          std::cout << "client say: " << buffer << std::endl;
-          ret = send(fd, buffer, ret, 0);
-        }
-        if (ret <= 0) {
-          if (errno == EAGAIN) continue;
-          close(fd);
-          break;
-        }
-      }
-    });
-  }
-  monsoon::IOManager::GetThis()->scheduler(watch_io_read);
+void watch_io_read()
+{
+    monsoon::IOManager::GetThis()->addEvent(listen_sock, monsoon::READ, test_accept);
 }
 
-void test_iomanager() {
-  int port = 8080;
-  struct sockaddr_in svr_addr;
-  // socklen_t cli_len = sizeof(cli_addr);
-  listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-  if (listen_sock < 0) {
-    std::cout << "creating listen socket error" << std::endl;
-    return;
-  }
+void test_accept()
+{
+    std::cout << "[server]: test_accept" << std::endl; 
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    socklen_t len = sizeof(addr);
+    int fd = accept(listen_sock, (struct sockaddr *)&addr, &len);
+    if (fd < 0)
+    {
+        std::cout << "[server]: fd = " << fd << ",accept error" << std::endl;
+    }
+    else
+    {
+        std::cout << "[server]: fd = " << fd << ",accept success" << std::endl;
+        fcntl(fd, F_SETFL, O_NONBLOCK);
+        monsoon::IOManager::GetThis()->addEvent(fd, monsoon::READ, [fd]()
+            {
+                char buffer[1024];
+                memset(buffer, 0, sizeof(buffer));
+                while (true)
+                {
+                    int ret = recv(fd, buffer, sizeof(buffer), 0);
+                    if (ret > 0)
+                    {
+                        std::cout << "client say: " << buffer << std::endl;
+                        ret = send(fd, buffer, ret, 0);
+                    }
+                    if (ret <= 0)
+                    {
+                        if (ret == -1 && errno == EAGAIN) 
+                        {
+                            std::cout << "[server]: nonblock--no data read!" << std::endl;
+                            continue;
+                        }
+                        std::cout << "[server]: recv error!" << std::endl;
+                        close(fd);
+                        break;
+                    }
+                } 
+            }
+        );
 
-  int opt = 1;
-  setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-  memset((char *)&svr_addr, 0, sizeof(svr_addr));
-  svr_addr.sin_family = AF_INET;
-  svr_addr.sin_port = htons(port);
-  svr_addr.sin_addr.s_addr = INADDR_ANY;
-
-  if (bind(listen_sock, (struct sockaddr *)&svr_addr, sizeof(svr_addr)) < 0) {
-    std::cout << "bind error" << std::endl;
-    return;
-  }
-
-  if (listen(listen_sock, 1024) < 0) {
-    std::cout << "listen error" << std::endl;
-    return;
-  } else {
-    std::cout << "listen success on port: " << port << std::endl;
-  }
-
-  fcntl(listen_sock, F_SETFL, O_NONBLOCK);
-
-  monsoon::IOManager iomanager;
-  iomanager.addEvent(listen_sock, monsoon::READ, test_accept);
+    }
+    monsoon::IOManager::GetThis()->scheduler(watch_io_read);
 }
 
-int main(int argc, char *argv[]) {
-  test_iomanager();
-  return 0;
+void test_iomanager()
+{
+    int port = 8080;
+    struct sockaddr_in svr_addr;
+
+    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock < 0)
+    {
+        std::cout << "creating listen socket error" << std::endl;
+        return;
+    }
+
+    int opt = 1;
+    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    memset((char *)&svr_addr, 0, sizeof(svr_addr));
+    svr_addr.sin_family = AF_INET;
+    svr_addr.sin_port = htons(port);
+    svr_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(listen_sock, (struct sockaddr *)&svr_addr, sizeof(svr_addr)) < 0)
+    {
+        std::cout << "bind error" << std::endl;
+        return;
+    }
+
+    if (listen(listen_sock, 1024) < 0)
+    {
+        std::cout << "listen error" << std::endl;
+        return;
+    }
+    else
+    {
+        std::cout << "listen success on port: " << port << std::endl;
+    }
+
+    fcntl(listen_sock, F_SETFL, O_NONBLOCK);
+
+    monsoon::IOManager iomanager;
+    iomanager.addEvent(listen_sock, monsoon::READ, test_accept);
+}
+
+int main(int argc, char *argv[])
+{
+    test_iomanager();
+
+    return 0;
 }
